9b9d5edec9411cfe82d1bd602273b9f0c68ff2bb
thongsavanhLaylani2023 <825133@mystma.org>
2021-12-16 08:54:36 -0600

    3

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-a = input("Please give me a word\n")
-b = input("Please give me a second word\n")
-
-def wordSmash(x, y):
-    return x + y 
-
-print(wordSmash(a, b))
+def wordSmash(a, b):
+    return a + b
